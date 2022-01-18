5e9d32a2bce2fc0bdf3bcbe4f48bca22947ee41f
IsmaelAlcantaraM <ismaelalcantaram@gmail.com>
2022-01-18 09:05:48 -0400

    Se agrega el Proyecto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // Bem Clas Naming convantion
+    <div className="app">
+      <h1>Lets build The MERN Tinder Clone guys!!!!</h1>
+
+      {/* Header*/}
+      {/*Tinder card*/}
+      {/*SwipeButtons */}
+      
+    </div>
+  );
+}
+
+export default App;
